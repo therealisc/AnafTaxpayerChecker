ee0cb7ebd1a5fd6532058a38c7aada1bac3eacd0
therealisc <85192263+therealisc@users.noreply.github.com>
2026-05-25 15:27:20 +0300

    Implement InfluxDB data writing for census points
    Added functionality to write census data points to InfluxDB with tags for location.

--- TaxpayerChecker/Pages/Privacy.cshtml.cs
+++ TaxpayerChecker/Pages/Privacy.cshtml.cs
@@ -23,5 +23,38 @@ public class PrivacyModel : PageModel
         var authToken = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN");
             
         using var client = new InfluxDBClient(hostUrl, token: authToken);
+
+        const string database = "<intercom_data>";
+
+        var points = new[]
+            {
+                PointData.Measurement("census")
+                    .SetTag("location", "Klamath")
+                    .SetField("bees", 23),
+                PointData.Measurement("census")
+                    .SetTag("location", "Portland")
+                    .SetField("ants", 30),
+                PointData.Measurement("census")
+                    .SetTag("location", "Klamath")
+                    .SetField("bees", 28),
+                PointData.Measurement("census")
+                    .SetTag("location", "Portland")
+                    .SetField("ants", 32),
+                PointData.Measurement("census")
+                    .SetTag("location", "Klamath")
+                    .SetField("bees", 29),
+                PointData.Measurement("census")
+                    .SetTag("location", "Portland")
+                    .SetField("ants", 40)
+            };
+
+        foreach (var point in points)
+        {
+            await client.WritePointAsync(point: point, database: database);
+    
+            Thread.Sleep(1000); // separate points by 1 second
+        }
+
+            Console.WriteLine("Complete. Return to the InfluxDB UI.");
     }
 }
