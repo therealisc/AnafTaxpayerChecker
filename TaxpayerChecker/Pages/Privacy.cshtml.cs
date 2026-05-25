c1fbca2ef07ef35712dc30cae93ce5bb2dc199df
therealisc <85192263+therealisc@users.noreply.github.com>
2026-05-25 15:45:38 +0300

    Update Privacy.cshtml.cs

--- TaxpayerChecker/Pages/Privacy.cshtml.cs
+++ TaxpayerChecker/Pages/Privacy.cshtml.cs
@@ -20,41 +20,16 @@ public class PrivacyModel : PageModel
     public void OnGet()
     {
         var hostUrl = "https://eu-central-1-1.aws.cloud2.influxdata.com";
+        
         var authToken = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN");
             
         using var client = new InfluxDBClient(hostUrl, token: authToken);
 
         const string database = "<intercom_data>";
 
-        var points = new[]
-            {
-                PointData.Measurement("census")
-                    .SetTag("location", "Klamath")
-                    .SetField("bees", 23),
-                PointData.Measurement("census")
-                    .SetTag("location", "Portland")
-                    .SetField("ants", 30),
-                PointData.Measurement("census")
-                    .SetTag("location", "Klamath")
-                    .SetField("bees", 28),
-                PointData.Measurement("census")
-                    .SetTag("location", "Portland")
-                    .SetField("ants", 32),
-                PointData.Measurement("census")
-                    .SetTag("location", "Klamath")
-                    .SetField("bees", 29),
-                PointData.Measurement("census")
-                    .SetTag("location", "Portland")
-                    .SetField("ants", 40)
-            };
+        const string record = "temperature,location=north value=60.0";
+        await client.WriteRecordAsync(record: record);
 
-        foreach (var point in points)
-        {
-            await client.WritePointAsync(point: point, database: database);
-    
-            Thread.Sleep(1000); // separate points by 1 second
-        }
-
-            Console.WriteLine("Complete. Return to the InfluxDB UI.");
+        Console.WriteLine("Complete. Return to the InfluxDB UI.");
     }
 }
