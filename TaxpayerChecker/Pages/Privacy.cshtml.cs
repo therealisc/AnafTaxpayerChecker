497cf2fb252e0644df1549d0670f7ef37d1bf5e5
therealisc <85192263+therealisc@users.noreply.github.com>
2026-05-25 15:47:55 +0300

    Update Privacy.cshtml.cs

--- TaxpayerChecker/Pages/Privacy.cshtml.cs
+++ TaxpayerChecker/Pages/Privacy.cshtml.cs
@@ -19,13 +19,11 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
-        var hostUrl = "https://eu-central-1-1.aws.cloud2.influxdata.com";
-        
+        var hostUrl = "https://eu-central-1-1.aws.cloud2.influxdata.com";        
         var authToken = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN");
-            
-        using var client = new InfluxDBClient(hostUrl, token: authToken);
-
         const string database = "<intercom_data>";
+        
+        using var client = new InfluxDBClient(hostUrl, token: authToken, database: database);
 
         const string record = "temperature,location=north value=60.0";
         await client.WriteRecordAsync(record: record);
